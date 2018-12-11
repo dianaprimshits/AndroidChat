57b1977d2a6cc5e842f014cfebf10994d5d62eb2
Diana Primshits <dianaprimshits@gmail.com>
2018-12-11 12:07:46 +0300

    Added simple map

--- app/src/main/java/com/bigsur/AndroidChatWithMaps/UI/maps/CustomMapView.java
+++ app/src/main/java/com/bigsur/AndroidChatWithMaps/UI/maps/CustomMapView.java
@@ -1,15 +1,30 @@
 package com.bigsur.AndroidChatWithMaps.UI.maps;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.View;
 
+import com.bigsur.AndroidChatWithMaps.R;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 
 public class CustomMapView extends MapView {
+    MapView mapView;
+
+    public MapView getMapView() {
+        return mapView;
+    }
+
     public CustomMapView(@NonNull Context context) {
         super(context);
     }
@@ -26,7 +41,36 @@ public class CustomMapView extends MapView {
         super(context, options);
     }
 
-    public void init() {
-
+    @Override
+    public void getMapAsync(@NonNull OnMapReadyCallback callback) {
+        super.getMapAsync(callback);
     }
+
+
+    public void init(Context context, Bundle savedInstanceState, View view) {
+        mapView = view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
+                                .target(new LatLng(53.9111266, 27.454437))
+                                .zoom(17)
+                                .tilt(45.0)
+                                .build()),
+                        10000);
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(53.9111266, 27.454437))
+                        .title("Heapix")
+                        .snippet("Minsk")
+                );
+
+                mapboxMap.getUiSettings().setAttributionEnabled(false);
+                mapboxMap.getUiSettings().setLogoEnabled(false);
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/bigsur/AndroidChatWithMaps/UI/maps/ItemMapsFragment.java
+++ app/src/main/java/com/bigsur/AndroidChatWithMaps/UI/maps/ItemMapsFragment.java
@@ -7,9 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.bigsur.AndroidChatWithMaps.R;
+import com.mapbox.mapboxsdk.Mapbox;
+
 
 public class ItemMapsFragment extends Fragment {
-    CustomMapView mapView;
 
     public static ItemMapsFragment newInstance() {
         ItemMapsFragment fragment = new ItemMapsFragment();
@@ -25,8 +26,11 @@ public class ItemMapsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        Mapbox.getInstance(getContext(), getString(R.string.mapbox_access_token));
         View view = inflater.inflate(R.layout.fragment_maps, container, false);
-        mapView.init();
+        CustomMapView mapView = new CustomMapView(getContext());
+        mapView.init(getContext(), savedInstanceState, view);
+
         return view;
     }
 }
